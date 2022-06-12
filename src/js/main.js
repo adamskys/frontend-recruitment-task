96745b5447687a803efabdc7efedbc3dfaeb3eff
Mario Krok-Adamski <mariokadamski@Marios-MacBook-Air.local>
2022-06-12 22:02:20 +0200

    modal functionality added with counting clicks

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,43 @@
+'use strict';
+
+// 1.
+
 const title = 'Mario Krok-Adamski - Recruitment task';
 document.querySelector('title').textContent = title;
+
+// 3.
+/*--  Selecting elements  --*/
+const popup = document.querySelector('.popup');
+const popupContainer = document.querySelector('.popup-container');
+const popupParagraph = document.querySelector('.popup-paragraph');
+const btnOpenPopup = document.querySelector('.button-popup');
+const btnClosePopup = document.querySelector('.close-popup');
+
+let userClickCount = 0;
+
+/*--  Opening and closing modal + counting clicks  --*/
+btnOpenPopup.addEventListener('click', () => {
+  popup.classList.remove('hidden');
+  userClickCount += 1;
+  popupParagraph.textContent = `You have clicked ${userClickCount} times to related button.`;
+})
+
+const closePopup = () => {
+  popup.classList.add('hidden');
+}
+
+btnClosePopup.addEventListener('click', () => {closePopup()});
+
+document.addEventListener('click', e => {
+  if(e.target.classList.contains('popup')) {
+    closePopup();
+  }
+});
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape') {
+    closePopup();
+  }
+});
+
+/*--  Counting and storing clicks  --*/
